e475466d4be357fd125caa6e32b96a6b9c800f85
Russkiy1389@gmail.com <Digital-Foundry.russkiy1389@gmail.com>
2023-02-24 23:51:03 -0500

    I am now stuck on figuring out how to add textures to my objects.

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createRoot } from 'react-dom/client'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { Suspense } from 'react'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import "./style.css";
 
-export default function App() {
+
+
+function Orb(props) {
+  const mesh = useRef();
+  
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+         <sphereGeometry args={[1, 32, 32]} />
+         <ambientLight intensity={0.2} />
+         <directionalLight />
+         {/* <meshStandardMaterial map={colorMap}/> */}
+      <meshStandardMaterial color={hovered ? 'green' : 'blue'} />
+    </mesh>
+        {/* const colorMap = useLoader(TextureLoader, 'assets/textures/lava_black_d.jpg') */}
+    </>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+createRoot(document.getElementById('root')).render(
+  <Canvas>
+    <Suspense fallback={null}>
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <Orb position={[0, 0, 0]} />
+    </Suspense>
+  </Canvas>,
+)
+
+
+export default App;
